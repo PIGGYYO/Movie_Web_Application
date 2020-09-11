d92e7278b895790bff6e6c28431f81f6304d9c23
ston253 <wuzhuwangpig@hotmail.com>
2020-09-11 16:41:09 +0800

    entry point

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+"""Application entry point."""
+# wsgi.py
+from movie_web_app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, threaded=False)
