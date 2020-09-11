17d3d51f3e942e337a7a09c2d853d5cc83914528
ston253 <wuzhuwangpig@hotmail.com>
2020-09-11 13:54:48 +0800

    app

--- movie_web_app/__init__.py
+++ movie_web_app/__init__.py
@@ -5,3 +5,8 @@ from flask import Flask, request
 def create_app():
     app = Flask(__name__)
 
+    # Configure the app from configuration-file settings
+    app.config.from_object('config.Config')
+
+    return app
+
